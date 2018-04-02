1e5e523350cf19e2dc9cbba7d27f4a370bfbd45a
playitouch@163.com <playitouch@163.com>
2018-04-03 00:08:24 +0800

    first commit

---
+++ dao/TextDao.cs
@@ -0,0 +1,33 @@
+﻿using domain;
+using IBatisNet.DataMapper;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dao
+{
+    public class TextDao
+    {
+        public IList<TextInfo> getTextList(int start, int page_size = 10)
+        {
+            try
+            {
+                //return Mapper.Instance().QueryForList<TextInfo>("getTextList", new object());                
+                //return Mapper.Get().QueryForList<TextInfo>("getTextList",
+                //    new PageParameter(start, page_size));
+                return Mapper.Get().QueryForList<TextInfo>("getTextList",
+                    new { start, page_size });
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+    }
+}
---
+++ service/TextService.cs
@@ -0,0 +1,21 @@
+﻿using dao;
+using domain;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace service
+{
+    public class TextService
+    {
+        TextDao textDao = new TextDao();
+
+        public IList<TextInfo> getTextList(int start, int page_size = 10)
+        {
+            return textDao.getTextList(start, page_size);
+        }
+    }
+}
